28b9914055498eaaf6a2ff58353a175df60bb1a4
pawankexeire <pawank@exeire.com>
2026-03-21 18:23:52 +0530

    testing

--- 02-control-flow/for_loop/main.go
+++ 02-control-flow/for_loop/main.go
@@ -1,15 +1,23 @@
 package main
 
+import "fmt"
+
+// func main() {
+// 	for i := range 5 {
+// 		fmt.Println(i)
+// 	}
+// }
+
+// func main() {
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
 func main() {
-	// for i := 0; i < 5; i++ {
-	// 	println(i)
-	// }
-	// i := 0
-	// for i < 5 {
-	// 	println(i)
-	// 	i++
-	// }
-	for i := range 10 {
-		println(i)
+	i := 0
+	for i < 5 {
+		fmt.Println(i)
+		i++
 	}
 }
