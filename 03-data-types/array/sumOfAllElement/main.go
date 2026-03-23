250542c420af5402a3e644f197f3eebf0f635462
pawankexeire <pawank@exeire.com>
2026-03-23 16:31:02 +0530

    Fixes

--- 03-data-types/array/sumOfAllElement/main.go
+++ 03-data-types/array/sumOfAllElement/main.go
@@ -1,13 +1,15 @@
 package main
 
-func main() {
-	num := [5]int{10, 20, 30, 40, 50}
+func sumOfAllElement(arr []int) int {
 	sum := 0
-	// for _, value := range num {
-	// 	sum += value
-	// }
-	for _, value := range num {
+	for _, value := range arr {
 		sum += value
 	}
+	return sum
+}
+
+func main() {
+	num := []int{10, 20, 30, 40, 50}
+	sum := sumOfAllElement(num)
 	println("Sum of all elements in array is ", sum)
 }
