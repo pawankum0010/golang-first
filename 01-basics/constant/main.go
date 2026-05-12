6860d02c8d2949aa8e49b1548522b0e3f4389477
Pawan Kumar <pawankum0010@gmail.com>
2026-05-12 13:44:42 +0530

    Add file for logger

--- 01-basics/constant/main.go
+++ 01-basics/constant/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"goLangFirst/utils/myutil"
+	"time"
+)
 
 const age int = 30
 const name string = "Pawan Kumar" //Pawan
@@ -12,10 +17,33 @@ func main() {
 		education string = "B.Tech"
 	)
 
-	fmt.Println("This is my age: ", age)
-	fmt.Println("This is my name: ", name)
-	fmt.Println("This is my city: ", city)
-	fmt.Println("This is my country: ", country)
-	fmt.Println("This is my education: ", education)
-	fmt.Println("This is my age: ", age)
+	logger, closeLogger, err := myutil.NewExecutionLogger()
+	if err != nil {
+		log.Fatalf("failed to setup logger: %v", err)
+	}
+	defer closeLogger()
+
+	programStart := time.Now()
+	logger.Println("program started")
+
+	myutil.RunStep(logger, "print age", func() {
+		fmt.Println("This is my age: ", age)
+	})
+	myutil.RunStep(logger, "print name", func() {
+		fmt.Println("This is my name: ", name)
+	})
+	myutil.RunStep(logger, "print city", func() {
+		fmt.Println("This is my city: ", city)
+	})
+	myutil.RunStep(logger, "print country", func() {
+		fmt.Println("This is my country: ", country)
+	})
+	myutil.RunStep(logger, "print education", func() {
+		fmt.Println("This is my education: ", education)
+	})
+	myutil.RunStep(logger, "print age again", func() {
+		fmt.Println("This is my age: ", age)
+	})
+
+	logger.Printf("program completed in %s", myutil.FormatDuration(time.Since(programStart)))
 }
