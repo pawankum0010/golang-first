7cc6cb8d44aa38ce6a37a3c9c18197dc668e2420
Pawan Kumar <pawankum0010@gmail.com>
2026-06-06 08:49:57 +0530

    Digit Count Occurrance

--- 03-data-types/array/countDigitOccurrance/main.go
+++ 03-data-types/array/countDigitOccurrance/main.go
@@ -11,14 +11,14 @@ func countDigitOccurrance(input string) map[rune]int {
 }
 
 func main() {
-	input := "1234561345753"
+	input := "123544562134541534152358246739569"
 	digitCount := countDigitOccurrance(input)
 	if len(digitCount) == 0 {
-		fmt.Println("No no found in string")
+		fmt.Println("No no found in this string")
 	}
 	for digit, count := range digitCount {
-		if count > 1 {
-			fmt.Printf("Digit %q occurs %d times\n", digit, count)
+		if count > 4 {
+			fmt.Printf("Digit %q has occurance %d\n", digit, count)
 		}
 	}
 }
